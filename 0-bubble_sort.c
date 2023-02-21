60e18aecf7b0bbeded21de219c9b2e5f3e7399e8
Oluwatobiloba <iamtobihunkuten@gmail.com>
2023-02-21 20:30:50 +0100

    fixes

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,19 +9,20 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j, c;
+	size_t i, j, k;
 
-	for (i = 0; i < size - 1; i++)
+	for (i = 0; i < size; i++)
 	{
-		for (j = 0; j < size - 1; j++)
+		for (j = 0; j < size - i - 1 ; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
-				c = array[j];
+				k = array[j];
 				array[j] = array[j + 1];
-				array[j + 1] = c;
+				array[j + 1] = k;
 				print_array(array, size);
 			}
 		}
-	} 
+
+	}
 }
