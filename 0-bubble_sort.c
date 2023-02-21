bc126c3d3f20caf436048d1f6f65ce44f70a8036
Oluwatobiloba <iamtobihunkuten@gmail.com>
2023-02-21 22:23:27 +0100

    fixed bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,28 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - the bubble sort algorithm
+ * @array:array pointer
+ * @size:size of the array
+ *
+ * Return:ir returns null Void
+ */
+ void bubble_sort(int *array, size_t size)
+{
+	size_t i, j, k;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size - i - 1 ; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				k = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = k;
+				print_array(array, size);
+			}
+		}
+
+	}
+}
